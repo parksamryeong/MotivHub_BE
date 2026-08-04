ee8324903ac0b601a4eb0c8646e01793d9bc2028
parksamryeong <shyp476@naver.com>
2026-08-04 15:49:50 +0900

    chore: Spring Boot 프로젝트 초기 설정
    - Gradle(Groovy), Java 21, Spring Boot 4.1.0
    - Web, JPA, MySQL, Security, Validation, Lombok, DevTools 의존성 추가

---
+++ src/main/java/com/motivhub/be/MotivhubBeApplication.java
@@ -0,0 +1,13 @@
+package com.motivhub.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivhubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivhubBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motivhub/be/MotivhubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motivhub.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivhubBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
